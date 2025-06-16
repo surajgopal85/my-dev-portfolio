728d1a48d605bfd78fdb00b81f9692447b9e5e8b
Suraj Gopal <surajcancode@gmail.com>
2025-06-15 21:13:11 -0500

    switchcase for scroll in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,43 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
 import './App.css'
-
-// my imports
+import React, { useRef } from 'react';
 import { About } from './About';
+import { TopNav } from './TopNav';
 import { PortfolioOverview } from './PortfolioOverview';
 import { Hero } from './Hero';
-import { SideNavSocials } from './SideNavSocials';
+// import { SideNavSocials } from './SideNavSocials';
 
 function App() {
+  const aboutRef = useRef<HTMLDivElement>(null);
+  const projectsRef = useRef<HTMLDivElement>(null);
+  const contactRef = useRef<HTMLDivElement>(null);
+
+  const scrollToSection = (id: string) => {
+    // init null, eventually references a div for scroll
+    let ref: React.RefObject<HTMLDivElement> | null = null;
+    // switchboard to handle :)
+    switch (id) {
+      case 'about':
+        ref = aboutRef;
+        break;
+      case 'project':
+        ref = projectsRef;
+        break;
+      case 'contact':
+        ref = contactRef;
+        break;
+      default:
+        break;
+    }
+
+    if (ref && ref.current) {
+      ref.current.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+  }
 
   return (
     <div className='min-h-screen bg-dark-bg text-light-text font-sans'>
-      <SideNavSocials />
+      <TopNav onNavLinkClick={scrollToSection}/>
+      {/* <SideNavSocials /> */}
       <Hero 
       name="Suraj"
       tagline="I build fast & accessible web apps with React & TypeScript."
@@ -21,7 +45,9 @@ function App() {
       scrollText='Scroll down to learn more.'
       // ctaHref="#projects"
       />
-      {/* <About /> */}
+      <div id='about' ref={aboutRef}>
+        <About />
+      </div>
       <PortfolioOverview />
     </div>
   )
