ad0e230551da1ac6d9dadb421df9f9d59d82e58a
Suraj Gopal <surajcancode@gmail.com>
2025-06-15 21:16:09 -0500

    switch overview to port-main

---
+++ src/PortfolioMain.tsx
@@ -0,0 +1,10 @@
+import { WorkCard } from "./WorkCard";
+import { motion } from 'framer-motion';
+
+export const PortfolioMain: React.FC = () => {
+    return (
+         <section className="py-24 px-4 sm:px-8 max-w-6xl mx-auto">
+            
+         </section>
+    )
+}
