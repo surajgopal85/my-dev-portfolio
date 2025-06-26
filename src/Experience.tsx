94676900715723ca1f441d5f546b12bba0e0ce14
Suraj Gopal <surajcancode@gmail.com>
2025-06-25 22:28:30 -0500

    experience timeline with bolded employers, text-xs descriptions prototyped

--- src/Experience.tsx
+++ src/Experience.tsx
@@ -2,44 +2,29 @@ export const Experience: React.FC = () => {
     return (
         <div className="flex flex-row">
             <div className="border-gray-200 mr-16">
-                <div className="text-black">BOX</div>
-                <div>BOX</div>
+                <div className="font-bold translate-y-0.5">Alliance for Young Artists & Writers</div>
+                <div><p className="text-xs">GatsbyJS, React, ChakraUI, AWS, Wordpress</p></div>
                 <div className="text-black">BOX</div>
                 <div>BOX</div>
                 <div className="text-black">BOX</div>
             </div>
             <div className="flex justify-center flex-col gap-0">
-                <div className="rounded-full w-4 h-4 outline-amber-100 outline-1 bg-blue-900"></div>
-                <div className="h-12 border-l-1 translate-x-2 border-amber-100"></div>
-                <div className="rounded-full w-4 h-4 outline-amber-100 outline-1 bg-blue-900"></div>
-                <div className="h-12 border-l-1 translate-x-2 border-amber-100"></div>
-                <div className="rounded-full w-4 h-4 outline-amber-100 outline-1 bg-blue-900"></div>
-                <div className="h-12 border-l-1 translate-x-2 border-amber-100"></div>
-                <div className="rounded-full w-4 h-4 outline-amber-100 outline-1 bg-blue-900"></div>
+                <div className="rounded-full w-4 h-4 outline-amber-100 outline-2 bg-blue-900"></div>
+                <div className="h-14 border-l-2 translate-x-2 border-amber-100"></div>
+                <div className="rounded-full w-4 h-4 outline-amber-100 outline-2 bg-blue-900"></div>
+                <div className="h-12 border-l-2 translate-x-2 border-amber-100"></div>
+                <div className="rounded-full w-4 h-4 outline-amber-100 outline-2 bg-blue-900"></div>
+                <div className="h-12 border-l-2 translate-x-2 border-amber-100"></div>
+                <div className="rounded-full w-4 h-4 outline-amber-100 outline-2 bg-blue-900"></div>
             </div>
-            {/* <ol className="relative border-s border-gray-200 dark:border-gray-700">
-                <div className="rounded-full w-4 h-4 bg-blue-900"></div>
-                <li className="mb-10 ms-4">
-                    <p className="text-black">text</p>
-                </li>
-                <li className="mb-10 ms-4">
-                    <p className="text-black">text</p>
-                </li>
-                <li className="mb-10 ms-4">
-                    <p className="text-black">text</p>
-                </li>
-            </ol> */}
-            {/* <ol className="relative border-e border-gray-200 dark:border-gray-700">
-                <li className="mb-10 ms-4">
-                    <p>text</p>
-            </li>
-            </ol> */}
             <div className="border-gray-200 ml-16">
                 <div>BOX</div>
                 <div className="text-black">BOX</div>
-                <div>BOX</div>
-                <div className="text-black">BOX</div>
-                <div>BOX</div>
+                {/* <div className="text-black">BOX</div> */}
+                <div className="mt-2">
+                    <div className="font-bold">NYC Department of Education</div>
+                    <div className="text-xs">AP Java, Python, Precalculus, Physics, Chemistry</div>
+                </div>
             </div>
         </div>
     );
