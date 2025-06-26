e18cfc19dbe3023349eec155ee76109b61b51632
Suraj Gopal <surajcancode@gmail.com>
2025-06-25 21:18:44 -0500

    divs with alternating text and margin, next empty circles

--- src/Experience.tsx
+++ src/Experience.tsx
@@ -1,15 +1,36 @@
 export const Experience: React.FC = () => {
     return (
-        <ol className="relative border-s border-gray-200 dark:border-gray-700">
-            <li className="mb-10 ms-4">
-                <p>text</p>
+        <div className="flex flex-row">
+            <div className="border-gray-200 mr-16">
+                <div>BOX</div>
+                <div>BOX</div>
+                <div>BOX</div>
+                <div>BOX</div>
+                <div>BOX</div>
+            </div>
+            <ol className="relative border-s border-gray-200 dark:border-gray-700">
+                <li className="mb-10 ms-4">
+                    <p>text</p>
+                </li>
+                <li className="mb-10 ms-4">
+                    <p>text</p>
+                </li>
+                <li className="mb-10 ms-4">
+                    <p>text</p>
+                </li>
+            </ol>
+            {/* <ol className="relative border-e border-gray-200 dark:border-gray-700">
+                <li className="mb-10 ms-4">
+                    <p>text</p>
             </li>
-            <li className="mb-10 ms-4">
-                <p>text</p>
-            </li>
-            <li className="mb-10 ms-4">
-                <p>text</p>
-            </li>
-        </ol>
+            </ol> */}
+            <div className="border-gray-200 ml-16">
+                <div>BOX</div>
+                <div className="text-black">BOX</div>
+                <div>BOX</div>
+                <div className="text-black">BOX</div>
+                <div>BOX</div>
+            </div>
+        </div>
     );
 }
