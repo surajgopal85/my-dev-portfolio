f2dad09f594c6256093409943c36a1df8af775dc
Suraj Gopal <surajcancode@gmail.com>
2025-09-08 16:55:37 -0500

    on card click bounce to workDetailsPage - description of tech used, code snippets included, learning insights

---
+++ src/WorkDetailsPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+type WorkDetailsPageProps = {
+    project: Project;
+}
+
+export const WorkDetailsPage: React.FC = () => {
+    return (
+        <div>
+            <h1>Work Details Page</h1>
+        </div>
+    )
+}
